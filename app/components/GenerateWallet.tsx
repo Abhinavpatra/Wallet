6dff2f5808e79a512b4fee0f10fd9388d2c506b5
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2024-10-04 21:50:03 +0530

    storing in local storage with wallet gaps and copying and encoding

--- app/components/GenerateWallet.tsx
+++ app/components/GenerateWallet.tsx
@@ -91,11 +91,11 @@ export function GenerateWallet({ mnemonic }: { mnemonic: string }) {
               <button onClick={() => {
                 handleCopy(key);
               }} >
-                <span id="default-message" className="ml-5 inline-flex items-center">
+                <span id="default-message" className="bg-slate-400 rounded-md ml-5 inline-flex items-center mb-2 pl-1 pr-1">
                   <svg className="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                     <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
                   </svg>
-                  <span className="text-s font-semibold">Copy</span>
+                  <span className="text-s font-thin">Copy</span>
                 </span>
               </button>
             </li>
@@ -103,8 +103,21 @@ export function GenerateWallet({ mnemonic }: { mnemonic: string }) {
         </ul>
         <ul>
           {privateKeys.map((key, index) => (
-            <li className="text-red-500 blur-lg hover:blur-none" key={index}>Wallet{index + 1}  {key}</li>
+            <li className="text-red-500 blur-lg hover:blur-none" key={index}>Wallet{index + 1}  {key}
+                <button onClick={() => {
+                handleCopy(key);
+              }} >
+                <span id="default-message" className="bg-slate-400 text-white rounded-md ml-5 inline-flex items-center mb-2 pl-1 pr-1">
+                  <svg className="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                    <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                  </svg>
+                  <span className="text-s font-thin">Copy</span>
+                </span>
+              </button>
+            </li>
+            
           ))}
+          
         </ul>
       </div>
     </>
